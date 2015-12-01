a2b0de2f968a49c4511cbd0f6c3b4404cfe85061
Yang Songxiang <yangsongxiang@caredear.com>
2015-12-01 10:55:51 +0800

    at this stage:
    * finish a full flow of models and db
    * learn a lot on Django

--- webabc/accounts/models.py
+++ webabc/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Account(models.Model):
+    user_name = models.CharField(max_length=30)
+    user_type = models.BigIntegerField(blank=True,null=True)
+    password = models.CharField(max_length=128)
+    active = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
